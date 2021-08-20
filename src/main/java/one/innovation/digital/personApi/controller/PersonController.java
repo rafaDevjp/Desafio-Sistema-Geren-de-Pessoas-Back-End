f8abd14d268beabadce0854d0ffcb38bfc14369c
Rafael Andrade <rafa.devjp@gmail.com>
2021-08-19 23:03:50 -0300

    first commit

---
+++ src/main/java/one/innovation/digital/personApi/controller/PersonController.java
@@ -0,0 +1,16 @@
+package one.innovation.digital.personApi.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/person/")
+public class PersonController {
+
+    @GetMapping
+    public String getBook() {
+        return "API RestFUll successfull";
+    }
+}
---
+++ src/test/java/one/innovation/digital/personApi/PersonApiApplicationTests.java
@@ -0,0 +1,13 @@
+package one.innovation.digital.personApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
